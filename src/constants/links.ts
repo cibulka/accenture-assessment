4d928c6855101f7a7821e432897bbf5d7f63c978
Petr Cibulka <cibulka.me@gmail.com>
2024-09-05 22:06:04 +0200

    fix: fix broken link to components library

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -2,6 +2,6 @@ export const LINKS = {
   cibulka: "https://www.cibulka.codes",
   cv: "https://www.cibulka.codes/cv.pdf",
   figma: "https://www.figma.com/design/uoopYB7BMtSOB0Jq37pvxz/Web-Components-Analysis",
-  storybook: "https://www.accenture-ui.cibulka.codes",
+  storybook: "https://accenture-ui.cibulka.codes",
   tetris: "https://www.cibulka.codes/tetris",
 };
